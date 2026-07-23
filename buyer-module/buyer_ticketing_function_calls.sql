f60e029e7969f4d66ac6f41d3474bc048fa0cb22
Madifa Nasir <241370162@gift.edu.pk>
2026-07-23 22:45:18 +0500

    Add files via upload
    Add buyer module ticketing functionality (Tasks 1-6)

--- buyer-module/buyer_ticketing_function_calls.sql
+++ buyer-module/buyer_ticketing_function_calls.sql
@@ -1,1 +1,13 @@
--- Write your function calls here
+-- Buyer Ticketing Function Calls
+
+-- 1. Browse Scheduled Events
+
+SELECT * FROM browse_scheduled_events();
+
+-- 2. View Tickets for a Specific Event
+
+-- View tickets for event 1
+SELECT * FROM view_event_tickets(1);
+
+-- View tickets for event 2
+SELECT * FROM view_event_tickets(2);
